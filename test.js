c73432243eadcfbf257f5954f157feedb5fec3af
nuynuy28 <azalea_kudo@yahoo.com>
2023-08-11 10:40:59 +0700

    File terdiri dari 2 file

---
+++ test.js
@@ -0,0 +1,16 @@
+let number = prompt('Masukan Angka:')
+let result = Math.sqrt(number)
+var notif;
+
+
+if (number%2!==0) {
+    notif = "Tidak bisa input bilangan ganjil";
+} 
+    else  {
+        alert ('Akar Kuadrat dari '+ number + ' adalah '+ result)
+            
+    }
+    alert(notif);
+ 
+
+
